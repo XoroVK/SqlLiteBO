de22de98c039ff0e2e3891b9ca81b91c6afb5cef
unknown <sistemas@CHI051PCAL003.alumnos.cst>
2024-10-29 12:38:58 -0300

    Initial commit

---
+++ app/src/main/java/com/cmsdev/sqllitebo/MainActivity.java
@@ -0,0 +1,93 @@
+package com.cmsdev.sqllitebo;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    Database database;
+    EditText  ingresarTareas, editarTareas;
+    Button btnAgregar, btnEditar, btnEliminar;
+    ListView listarTareas;
+    ArrayList<String> tareas;
+    ArrayAdapter<String> adapter;
+    String seleccionarTareas;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        database = new Database(this);
+        ingresarTareas = findViewById(R.id.editTextTask);
+        editarTareas = findViewById(R.id.editTextEditTask);
+        btnAgregar = findViewById(R.id.buttonAdd1);
+        btnEditar = findViewById(R.id.buttonEdit);
+        btnEliminar = findViewById(R.id.buttonDelete);
+        listarTareas = findViewById(R.id.listViewTasks);
+        tareas = database.getAllTareas();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, tareas);
+        listarTareas.setAdapter(adapter);
+
+        btnAgregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String tarea = ingresarTareas.getText().toString();
+                if(!tarea.isEmpty()){
+                    database.insertTarea(tarea);
+                    actualizarListadoTareas();
+                    ingresarTareas.setText("");
+                }
+            }
+        });
+        listarTareas.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                seleccionarTareas = tareas.get(position);
+                editarTareas.setText(seleccionarTareas);
+                editarTareas.setVisibility(View.VISIBLE);
+                btnEditar.setVisibility(view.VISIBLE);
+                btnEliminar.setVisibility(view.VISIBLE);
+            }
+        });
+        btnEditar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String nuevaTarea = editarTareas.getText().toString();
+                if(!nuevaTarea.isEmpty()){
+                    database.updateTarea(seleccionarTareas,nuevaTarea);
+                    actualizarListadoTareas();
+                    LimpiarCampos();
+                }
+            }
+        });
+        btnEliminar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                database.deleteTarea(seleccionarTareas);
+                actualizarListadoTareas();
+                LimpiarCampos();
+            }
+        });
+    }
+    public void actualizarListadoTareas(){
+        tareas.clear();
+        tareas.addAll(database.getAllTareas());
+        adapter.notifyDataSetChanged();
+    }
+    public void LimpiarCampos(){
+        editarTareas.setText("");
+        editarTareas.setVisibility(View.GONE);
+        btnEditar.setVisibility(View.GONE);
+        btnEliminar.setVisibility(View.GONE);
+    }
+}
